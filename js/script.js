86defeb98f98792ab10b2b9a2f4b5d9443765806
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-03-07 20:29:15 +0900

    fix: first viewのスライダー実装

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function(){
+	// ヘッダーの背景色切り替え
 	$(window).on('scroll', function() {
 		if ($('.header__image').height() < $(this).scrollTop()) {
 			$('.header__contents').addClass('change-color');
@@ -7,9 +8,15 @@ $(function(){
 		}
 	});
 
+	// ハンバーガーメニュー
 	$('.burger-btn, .nav-item a').on('click',function(){
 		$('.burger-btn').toggleClass('close');
 		$('.header__nav-wrapper').fadeToggle(500);
 		$('body').toggleClass('noscroll');
+	});
+
+	// トップページFirstViewのスライドショー
+	$('.bg-slider').bgSwitcher({
+		images: ['images/fv-bgi_01@2x.jpg','images/fv-bgi_02@2x.jpg'], // 切り替える背景画像を指定
 	});
 });
